3792a1d62d84b2e75e92506d9b1a3185aeee7363
Haelinos <hznieland@outlook.com>
2025-03-07 15:25:19 +0100

    More placeholders

---
+++ Assets/Scripts/GenerateOrder.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateOrder : MonoBehaviour
+{
+    public GameObject banana;
+    public GameObject strawberries;
+    public GameObject blueberries;
+    public GameObject mango;
+    public GameObject spinach;
+
+    public bool smoothie1;
+    public bool smoothie2;
+    public bool smoothie3;
+    public bool smoothie4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Shows order on the screen
+    void ShowOrder() 
+    { 
+
+    }
+}
