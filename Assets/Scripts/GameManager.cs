ca1acdbe3355acfc0e5e718c1f73048f52a04d28
Haelinos <hznieland@outlook.com>
2025-03-12 17:45:00 +0100

    added game manager class for adding up points

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int points { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddPoints(int amount)
+    {
+        points += amount;
+    }
+
+    public void ResetPoints()
+    {
+        points = 0;
+    }
+}
